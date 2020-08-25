966320aadd6912501eb4a02deef3599b7628d996
mightycrazyfoo <curtrightd@outlook.com>
2020-08-25 13:38:16 -0600

    create, destroy sessions and user login logout, signup created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
   end
 
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -21,7 +21,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created successfully!"
       redirect_to @article
